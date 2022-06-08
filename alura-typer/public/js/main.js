d85608bf6203e394ec7ba171475994119259fcc1
Paulo-Dinis <paulo.dinis1996@gmail.com>
2022-06-08 16:01:59 -0300

    Estudos 08-06

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,30 +1,88 @@
-let frase = $('.frase').text();
-let numPalavras = frase.split(" ").length;
-let tamanhoFrase = $("#tamaho-frase");
-tamanhoFrase.text(numPalavras);
-
+let tempoInicial = $("#tempo").text();
 let campo = $(".campo-digitacao");
-campo.on("input",function(){
-    let conteudo = campo.val();
-    
-    let qtdPalavras = conteudo.split(/\S+/).length -1;
-    let caracteres = $('#contador-palavras').text(qtdPalavras);
+let reiniciarButton = $("#reiniciar");
 
-    let qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+
+$(function(){
+    atualizaFrase();
+    iniciaContadores();
+    iniciarCronometro();
+    comparaFrase();
+    $("#reiniciar").click(reinicializar);
 });
 
-var tempoRestante = $("#tempo").text();
-campo.one("focus", function(){
-    let cronometroId = setInterval(function(){
-        tempoRestante --;
-        $("#tempo").text(tempoRestante);
-        if(tempoRestante == 0){
-            campo.attr("disabled", true);
-            clearInterval(cronometroId);
+function atualizaFrase(){
+    let frase = $('.frase').text();
+    let numPalavras = frase.split(" ").length;
+    let tamanhoFrase = $("#tamaho-frase");
+    tamanhoFrase.text(numPalavras);
+};
+
+function iniciaContadores(){
+    campo.on("input",function(){
+        let conteudo = campo.val();
+        
+        let qtdPalavras = conteudo.split(/\S+/).length -1;
+        let caracteres = $('#contador-palavras').text(qtdPalavras);
+
+        let qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+};
+
+function iniciarCronometro(){
+        var tempoRestante = $("#tempo").text();
+        campo.one("focus", function(){
+            let cronometroId = setInterval(function(){
+            tempoRestante --;
+            $("#tempo").text(tempoRestante);
+            if(tempoRestante < 1){
+                campo.attr("disabled", true);
+                clearInterval(cronometroId);
+                campo.toggleClass("campo-desativado");
+            }
+            if(tempoRestante > 0){
+                reiniciarButton.attr("disabled", true);
+            };
+            if(tempoRestante == 0){
+                reiniciarButton.addClass("animacao-botao");
+                reiniciarButton.attr("disabled", false);
+            };
+            
+        },1000);
+    });
+};
+
+function comparaFrase(){
+    let frase = $(".frase").text();
+    campo.on("input", function(){
+        let digitado = campo.val();
+        let comparavel = frase.substr(0,digitado.length);
+        if(digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }else{
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
         }
-    },1000);
-});
+    });
+}
+
+
+function reinicializar(){  
+    campo.attr("disabled", false);
+    campo.val("");
+    $('#contador-palavras').text("0");
+    $('#contador-caracteres').text("0");
+    $("#tempo").text(tempoInicial);
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-verde");
+    campo.removeClass("borda-vermelha");
+    reiniciarButton.removeClass("animacao-botao");
+    iniciarCronometro();
+};
+
+
 
 
 
