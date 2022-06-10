08acfaf60fac629b680f14e75fc5ebc65bdea474
Paulo-Dinis <paulo.dinis1996@gmail.com>
2022-06-10 13:44:46 -0300

    Estudos 10-06

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -2,7 +2,6 @@ let tempoInicial = $("#tempo").text();
 let campo = $(".campo-digitacao");
 let reiniciarButton = $("#reiniciar");
 
-
 $(function(){
     atualizaFrase();
     iniciaContadores();
@@ -72,16 +71,37 @@ function comparaFrase(){
 };
 
 function inserePlacar(){
-    let corpoTabela = $(".placar").find("tbody"); 
+    let corpoTabela = $("#placar").find("tbody"); 
     let usuario = "Paulo";
     let numPalavras = $("#contador-palavras").text();
 
-    let linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                "</tr>";
+    let linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha)
 
     corpoTabela.prepend(linha);
+};
+
+function novaLinha(usuario, palavras){
+    let linha = $("<tr>");
+    let colunaUser = $("<td>").text(usuario);
+    let colunaPalavras = $("<td>").text(palavras);
+    let colunaRemover = $("<td>");
+
+    let link = $("<a>").addClass("botao-remover").attr("href","#");
+    let icone = $("<i>").addClass("small").addClass("material-icons").text("delete")
+
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUser);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+};
+
+function removeLinha(){
+    event.preventDefault();
+    $(this).parent().parent().remove();
 };
 
 
