f34c5e9cf9c30114c1a71ccba0f22351eace3c61
Paulo-Dinis <paulo.dinis1996@gmail.com>
2022-07-01 14:40:33 -0300

    Estudos 01-07

---
+++ jQuery/parte2/js/aula10/aula01.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".numeros").bind("keydown", function(e){
+        let keyCode = e.which;
+        let outraTecla = (",8,46,37,38,39,40,".indexOf(","+ keyCode +",") >-1);
+        let enumero = (
+            keyCode > 47 && keyCode < 58 || 
+            keyCode > 95 && keyCode < 106
+        );       
+        if(enumero || outraTecla){
+            return true;
+        }else{
+            return false;
+        }
+    });
+});
